6bab5011fd7e9e715d1d49850d1a3dcdedc6df38
Olivia <oliviawolfie@pm.me>
2026-05-06 17:35:11 +0200

    Clean up unnecessary and unused copy of SSLConfig that was moved to SocketConfig

--- src/raven/SocketServer.hpp
+++ src/raven/SocketServer.hpp
@@ -2,7 +2,6 @@
 
 #include "raven/ConnectionPool.hpp"
 #include "raven/Socket.hpp"
-#include "raven/config/SSLConfig.hpp"
 #include <memory>
 
 namespace raven {
@@ -13,8 +12,6 @@ struct ServerConfig {
     size_t threads = std::max<size_t>(
         1, std::thread::hardware_concurrency() / 2
     );
-
-    std::optional<SSLConfig> sslConfig;
 };
 
 class SocketServer {
